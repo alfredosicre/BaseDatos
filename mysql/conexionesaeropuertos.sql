d9ea4bad5d1fe8391a5c19d4d287f320e43e3605
alfredosicre <alfredo.sicre@gmail.com>
2025-05-09 13:52:33 +0200

    003

--- mysql/conexionesaeropuertos.sql
+++ mysql/conexionesaeropuertos.sql
@@ -3,3 +3,9 @@ select origen.idaeropuerto id_origen, origen.aeropuerto origen, destino.idaeropu
 	from aeropuertos origen
 	join conexiones cx on origen.idaeropuerto = cx.fk_aeropuerto_origen
 	join aeropuertos destino on destino.idaeropuerto = cx.fk_aeropuerto_destino;
+
+select * from aeropuertos as origen
+	join conexiones cx on cx.fk_aeropuerto_origen = origen.idaeropuerto
+    join aeropuertos as destino on cx.fk_aeropuerto_destino = destino.idaeropuerto;
+    
+    
